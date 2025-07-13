103add237b6e3c37d7ea96c6d7c2fb173b19bf24
kisoqual <64285271+kisoqual@users.noreply.github.com>
2025-07-13 13:46:07 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string.h>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+//using std::cout, std::cin, std::endl; // нужон C++17 для этого
+
+const char i[6] = " RYTP"; 
+char j[256] = "";
+
+int main()
+{
+    system("chcp 1251>NUL");
+    cout << "какой ты RYTP хочеш? ";
+    cin >> j;
+    cout << j << i << endl;
+}
