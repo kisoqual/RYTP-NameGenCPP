f58f7ee66823b12e212d9244709a05031b3e7078
kisoqual <64285271+kisoqual@users.noreply.github.com>
2025-07-14 18:59:03 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,22 +8,32 @@ using std::endl;
 
 //using std::cout, std::cin, std::endl; // нужон C++17 для этого
 
-int main(){
-    system("chcp 1251>NUL");
-    int v;
+int v;
+
+void select() {
     cout << "какой ты версию хочеш? ";
-    cin>>v;
-    if (v=1){
-        const char i[6] = " RYTP"; 
+    cin >> v;
+}
+
+int main() {
+    system("chcp 1251>NUL");
+    select();
+    if (v == 1) {
+        const char i[6] = " RYTP";
         char j[256];
         cout << "какой ты RYTP хочеш? ";
         cin >> j;
-        cout << j << i << endl;
+        cout << "\n" << j << i << endl;
+        cout << "\nНажмите на любую клавишу чтобы выйти..." << endl;
+        system("pause>NUL");
         return 0;
-    }
-    if (v=2){
+    } if (v == 2) {
         lulz(" RYTP");
         return 0;
+    }
+    else {
+        cout << "\aтакой версии нет" << endl;
+        select();
     }
     return 0; //можно не писать, но лучше на всякий случай
 }
