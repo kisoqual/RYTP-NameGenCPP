4afe0b9c31558b929ebd1604138e112e7ceeea2c
kisoqual <64285271+kisoqual@users.noreply.github.com>
2025-07-14 17:18:36 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,12 +13,16 @@ int main(){
     cout << "какой ты версию хочеш? ";
     cin>>v;
     if (v=1){
+        const char i[6] = " RYTP"; 
+        char j[256];
         cout << "какой ты RYTP хочеш? ";
         cin >> j;
         cout << j << i << endl;
+        return 0;
     }
     if (v=2){
-        lulz(" RYTP")
+        lulz(" RYTP");
+        return 0;
     }
     return 0; //можно не писать, но лучше на всякий случай
 }
