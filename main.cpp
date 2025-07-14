81aa9d9c71e5890ccbbd6abf5be1b9cb94222de9
kisoqual <64285271+kisoqual@users.noreply.github.com>
2025-07-14 17:11:36 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "add.h"
 
 using std::cout;
 using std::cin;
@@ -6,14 +7,18 @@ using std::endl;
 
 //using std::cout, std::cin, std::endl; // нужон C++17 для этого
 
-const char i[6] = " RYTP"; 
-char j[256];
-
-int main()
-{
+int main(){
     system("chcp 1251>NUL");
-    cout << "какой ты RYTP хочеш? ";
-    cin >> j;
-    cout << j << i << endl;
+    int v;
+    cout << "какой ты версию хочеш? ";
+    cin>>v;
+    if (v=1){
+        cout << "какой ты RYTP хочеш? ";
+        cin >> j;
+        cout << j << i << endl;
+    }
+    if (v=2){
+        lulz(" RYTP")
+    }
     return 0; //можно не писать, но лучше на всякий случай
 }
