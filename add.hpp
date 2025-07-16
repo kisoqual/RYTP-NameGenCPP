d9834625c5807961e6229599f71f2860e63c1f17
kisoqual <64285271+kisoqual@users.noreply.github.com>
2025-07-16 05:28:01 +0300

    Update add.hpp

--- add.hpp
+++ add.hpp
@@ -1,4 +1,3 @@
-//v2
 #pragma once
 #include <iostream>
 #include <string>
@@ -11,22 +10,22 @@ using std::string;
 
 //using std::cout, std::cin, std::endl, std::string; //нужон C++17
 
-void lulz(const string& i = "") {
+void lulz(const string& i) {
     cout << "какой ты RYTP хочеш? ";
     string j;
     cin >> j;
-    cout << "\n" << j + i << endl;
+    cout << "\n" << j << i << endl;
     cout << "\nНажмите на любую клавишу чтобы выйти..." << endl;
     system("pause>NUL");
 }
 
 //если надумаете заюзать массив из char вместо string
 /*
-void lulz(const char* i = "") {
+void lulz(const char* i) {
     char j[256];
     cout << "какой ты RYTP хочеш? ";
-    cin.getline(j, sizeof(j)); // указываем размер буфера
-    cout << "\n" << j + i << endl;
+    cin >> j;
+    cout << "\n" << j << i << endl;
     cout << "\nНажмите на любую клавишу чтобы выйти..." << endl;
     system("pause>NUL");
 }
